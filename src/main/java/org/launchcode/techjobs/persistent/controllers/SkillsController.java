d4a917ecff12026cf284eaa3a72df4636a6db300
Carissa Nelson <johnsonelijah440@gmail.com>
2024-02-13 21:41:54 -0600

    <GA4>

---
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillsController.java
@@ -0,0 +1,58 @@
+package org.launchcode.techjobs.persistent.controllers;
+
+import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skill")
+public class SkillsController {
+    @Autowired
+    SkillRepository skillRepository;
+
+    @GetMapping("skill")
+    public String index (Model model)
+    {
+        model.addAttribute("jobs", skillRepository.findAll() );
+        return "skill/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddEmployerForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skill/add";
+    }
+
+    @PostMapping("add")
+    public String processAddEmployerForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skill/add";
+        }
+                SkillRepository.save();
+                return "redirect:view";
+    }
+
+    @GetMapping("view/{employerId}")
+    public String displayViewEmployer(Model model, @PathVariable int skillId) {
+
+        Optional optSkill = SkillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Skill skl  = (Skill) optSkill.get();
+            model.addAttribute("skill", skl);
+            return "skill/view";
+        } else {
+            return "redirect:../";
+        }
+
+    }
+}
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
+
+    public Employer()
+    {
+
+    }
+
+    @NotEmpty
+    @Size(min = 2, max = 50)
+    String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
